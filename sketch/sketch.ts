876b7c7fd2343b2b9d355c5e6cd4712e80b611b5
Jules Fouchy <jules.fouchy@ntymail.com>
2020-10-09 13:10:05 +0200

    Properly center the canvas when it's not fullscreen ; refactor the choice of canvas size

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,11 +1,36 @@
+let canvas: p5.Renderer // Need to access this each time we resize the window, to center the canvas
+
 function setup() {
-    createCanvas(windowWidth, windowHeight)
+    canvas = createCanvas(desiredCanvasWidth(), desiredCanvasHeight())
+    canvas.position((windowWidth - width)/2, (windowHeight - height)/2) // Center the canvas
 }
 
 function draw() {
+    background(0)
     ellipse(mouseX, mouseY, 30)
 }
 
+
+// -------------------
+// Choose how your canvas will fit in the window
+// -------------------
+// Fill the window
+/*function desiredCanvasWidth(): number {
+    return windowWidth;
+}
+function desiredCanvasHeight(): number {
+    return windowHeight;
+}*/
+// Squared canvas with a margin
+function desiredCanvasWidth(): number {
+    const margin = 25 // in pixels
+    return min(windowWidth, windowHeight) - margin * 2;
+}
+function desiredCanvasHeight(): number {
+    return desiredCanvasWidth();
+}
+// -------------------
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight)
+    resizeCanvas(desiredCanvasWidth(), desiredCanvasHeight())
+    canvas.position((windowWidth - width)/2, (windowHeight - height)/2) // Center the canvas
 }
