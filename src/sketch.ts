930b3c7644a7edbcd9280ffa9078a83eb76d377c
Jules Fouchy <jules.fouchy@ntymail.com>
2021-01-31 18:23:15 +0100

    Fix image export

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,13 +1,48 @@
 
+let posX = 0
+let saving = false
+
+// Hello
+const mySave = function(filename: string, extension: string) {
+    noLoop()
+    let mimeType;
+    switch (extension) {
+      default:
+        //case 'png':
+        mimeType = 'image/png';
+        break;
+      case 'jpeg':
+      case 'jpg':
+        mimeType = 'image/jpeg';
+        break;
+    }
+    // @ts-ignore
+    canvas.elt.toBlob((blob, err) => {
+        // @ts-ignore
+      p5.prototype.downloadFile(blob, filename, extension);
+      loop();
+    }, mimeType);
+  };
+
 function setup() {
     template_CreateCanvas()
 }
 
 function draw() {
     background(0)
-    ellipse(mouseX, mouseY, 30)
+    ellipse(posX, height/2, 30)
+    posX += 1
+    if (frameCount < 200)
+        mySave(nf(frameCount, 3), "png")
 }
 
 function windowResized() {
-    template_ResizeCanvas()
+    //template_ResizeCanvas()
+}
+
+function keyPressed() {
+    if (key==' ') {
+        saving = !saving
+        loop()
+    }
 }
